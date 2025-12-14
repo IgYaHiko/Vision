b3d32812a7043e2bb3d8876c217fc04e6e7a7f8d
IgYaHiko <subhrokolay2@gmail.com>
2025-12-14 13:25:17 +0530

    19:billing-setup-done

--- src/app/api/billing/webhook/route.ts
+++ src/app/api/billing/webhook/route.ts
@@ -1,5 +1,44 @@
-import { NextRequest } from "next/server";
-
+import { NextRequest, NextResponse } from "next/server";
+import { validateEvent, WebhookVerificationError } from "@polar-sh/sdk/webhooks"
+import { isPolarWebhookEvents } from "@/lib/polar";
+import { PolarWebhookEventsProps } from "@/types/polar";
+import { inngest } from "@/inngest/client";
 export async function POST(req:NextRequest) {
+  const secret = process.env.POLAR_WEBHOOK_SECRET ?? ""
+  if(!secret) {
+     return new NextResponse('missing polar webhook secret in env, check env file', {status: 500})
+  }
+  const raw = await req.arrayBuffer()
+  const headerObject = Object.fromEntries(req.headers)
+  let verified: unknown
+  try {
+    verified = validateEvent(Buffer.from(raw),headerObject, secret)
+
+  } catch (error) {
+    if(error instanceof WebhookVerificationError) {
+         return new NextResponse("Invalide signature",{status: 403})
+    }
+    throw error;
+  }
+  if(!isPolarWebhookEvents(verified)) {
+     return new NextResponse("UnSupported Event", {status: 400})
+
+  }
+
+  const evt: PolarWebhookEventsProps = verified;
+  const id = String(evt.id ?? Date.now())
+
+  try {
+    await inngest.send({
+        name: "polar/webhook.received",
+        id,
+        data: evt,
+    })
+  } catch (error) {
+    console.log(error)
+    return new NextResponse("failed to process webhook", {status: 500})
+  }
+
+  return NextResponse.json({ok: true})
     
 }
--- src/components/button/subscription/subscribe-button.tsx
+++ src/components/button/subscription/subscribe-button.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { Button } from '@/components/ui/button'
 import { useSubscribePlan } from '@/hooks/use-billing';
+import { Loader2 } from 'lucide-react';
 import React from 'react'
 
 interface Props {
@@ -14,8 +15,22 @@ const SubscribeButton = ({onClick,disable}: Props) => {
     type='button'
     onClick={onSubscribe}
     disabled={isFetching}
-     variant={'ghost'} className='font-mono font-black'>
-        Subscribe
+     variant={'default'} className='font-mono font-black bg-[#3B66E8] dark:text-white hover:bg-blue-400'>
+      {
+        isFetching ? 
+
+        ( 
+           <>
+           <Loader2 className='animate-spin opacity-70' />
+           <span>Processing...</span>
+           </>
+           
+        ) : (
+           <>
+           Subscribe
+           </>
+        )
+      }
     </Button>
   )
 }
